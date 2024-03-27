22f797bd4bd00319b80326b22e35cd918e2c86ec
Myuanxi <myuanxi0507@gmail.com>
2024-03-27 12:32:04 +0800

    第四次作业

---
+++ assignment4/task1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+
+namespace AlarmClock
+{
+    public delegate void AlarmClockEventHandler(object sender, AlarmClockArgs e);
+    public class AlarmClockArgs : EventArgs
+    {
+        int time;
+        public int Time
+        {
+            get => time;
+        }
+        public AlarmClockArgs(int time)
+        {
+            this.time = time;
+        }
+    }
+
+    public class AlarmClock : IDisposable
+    {
+        int time;
+        System.Timers.Timer timer;
+        public event AlarmClockEventHandler Tick;
+        public event AlarmClockEventHandler Alarm;
+        List<int> alarmTimeList;
+
+        public AlarmClock(List<int> alarmTimeList)
+        {
+            time = 0;
+            timer = new System.Timers.Timer();
+            timer.Interval = 1000;
+            timer.Elapsed += TimerElapsed;
+            this.alarmTimeList = alarmTimeList;
+        }
+        private void TimerElapsed(object? sender, ElapsedEventArgs e)
+        {
+            time++;
+            AlarmClockArgs args = new AlarmClockArgs(time);
+            Tick?.Invoke(this, args);
+            if (alarmTimeList.Contains(time))
+            {
+                Alarm?.Invoke(this, args);
+            }
+        }
+        public void Start()
+        {
+            timer.Start();
+        }
+
+        public void Dispose()
+        {
+            timer?.Dispose();
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入闹钟响铃的次数：");
+            int count;
+            while (!int.TryParse(Console.ReadLine(), out count) || count < 0)
+            {
+                Console.WriteLine("输入无效，请输入一个正整数：");
+            }
+            
+            List<int> alarmTimeList = new List<int>();
+            Console.WriteLine("请依次输入响铃时间（整数）：");
+            for (int i = 0; i < count; i++)
+            {
+                int alarmTime;
+                while (!int.TryParse(Console.ReadLine(), out alarmTime) || alarmTime < 0)
+                {
+                    Console.WriteLine("输入无效，请输入一个正整数：");
+                }
+                alarmTimeList.Add(alarmTime);
+            }
+
+            using (AlarmClock alarm = new AlarmClock(alarmTimeList))
+            {
+                alarm.Tick += AlarmTick;
+                alarm.Alarm += AlarmAlarm;
+                alarm.Start();
+                Console.WriteLine("按任意键退出...");
+                Console.ReadKey();
+            }
+        }
+
+        private static void AlarmTick(object sender, AlarmClockArgs e)
+        {
+            Console.WriteLine($"Tick. 时间:{e.Time}");
+        }
+        private static void AlarmAlarm(object sender, AlarmClockArgs e)
+        {
+            Console.WriteLine($"Alarm! 时间:{e.Time}");
+        }
+    }
+}
---
+++ assignment4/task2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace GenericList
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Value { get; set; }
+        public Node(T t)
+        {
+            // 初始化节点
+            Next = null;
+            Value = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        // 添加元素到链表尾部
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        // 对每个元素执行操作
+        public void ForEach(Action<T> action)
+        {
+            Node<T> n = head;
+            while (n != null)
+            {
+                action(n.Value);
+                n = n.Next;
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            for (int i = 0; i < 30; i++)
+            {
+                list.Add(i + 1);
+            }
+
+            Console.Write("链表元素为：");
+            list.ForEach(m => Console.Write(m + " "));
+            Console.WriteLine();
+
+            // 防止链表为空时操作导致异常
+            if (list.Head != null)
+            {
+                Console.Write("最大值为：");
+                int max = list.Head.Value;
+                list.ForEach(m => { if (m > max) max = m; });
+                Console.WriteLine(max);
+
+                Console.Write("最小值为：");
+                int min = list.Head.Value;
+                list.ForEach(m => { if (m < min) min = m; });
+                Console.WriteLine(min);
+
+                Console.Write("和为：");
+                int sum = 0;
+                list.ForEach(m => sum += m);
+                Console.WriteLine(sum);
+            }
+            else
+            {
+                Console.WriteLine("链表为空。");
+            }
+        }
+    }
+}
